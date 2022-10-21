fc2e82663aec0fdcf47ecc6d377c504c54d6bc91
Pierre-François Salmand <maabaa@gmail.com>
2022-10-21 11:49:29 +0700

    Add nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,11 @@
-require('base')
-require('plugins')
+require('pefington.base')
+require('pefington.highlights')
+require('pefington.maps')
+require('pefington.plugins')
+
+local has = vim.fn.has
+local is_win = has "win32"
+
+if is_win then
+  require('pefington.windows')
+end
