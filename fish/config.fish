2d336c59370a33b47625f182720dbf3eef40c15a
Pierre-François Salmand <maabaa@gmail.com>
2022-12-06 15:18:23 +0700

    Switch to Starship

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,9 @@
 if status is-interactive
-    set -g async_prompt_functions _pure_prompt_git
 end
 
 # pnpm
 set -gx PNPM_HOME "/home/pef/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
+
+starship init fish | source
