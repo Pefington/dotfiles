0c3d467b3f1f047501158550b7bb824b763996e4
Pierre-François Salmand <maabaa@gmail.com>
2022-12-06 13:16:04 +0700

    Setup PNPM

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     set -g async_prompt_functions _pure_prompt_git
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/pef/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
